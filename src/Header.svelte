54c7c3d1f836bba168ebd648152d368bc6652518
fl4viooliveira <fl4viooliveira@gmail.com>
2021-04-17 22:07:09 +0100

    header Layout
    Was fix the objects on header to start the polishment and apply resposive menu

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,16 +1,23 @@
 
 <div class="container">    
-    
     <nav>
-        <h1 class="logo">Blogr</h1>                
+        <a href="#0" class="logo">
+            <img src="../images/logo.svg" alt="logo">
+        </a>               
         <ul>
-            <li>Product</li>
-            <li>Company</li>
-            <li>Connect</li>
+            <li>
+                <a href="#0">Product<img src="../images/icon-arrow-light.svg" alt="arrow"></a>
+            </li>
+            <li>
+                <a href="#0">Company<img src="../images/icon-arrow-light.svg" alt="arrow"></a>
+            </li>
+            <li>
+                <a href="#0">Connect<img src="../images/icon-arrow-light.svg" alt="arrow"></a>
+            </li>
         </ul>
         <div class="btn-nav">
-            <button>Login</button>
-            <button>Sign Up</button>
+            <button><a href="#0">Login</a></button>
+            <button><a href="#0">Sign Up</a></button>
         </div>
     </nav>
     <main>
@@ -20,78 +27,127 @@
         <p>Grow your audience and build your online brand</p>
     </main>
     <div class="btn-botton">
-        <button class="btn">Start for Free</button>
-        <button class="btn">Learn More</button>
+        <button><a href="#0">Start for Free</a></button>
+        <button><a href="#0">Learn More</a></button>
     </div>
 </div>
 
 <style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
 
     @import url('https://fonts.googleapis.com/css2?family=Overpass:wght@300;600&family=Ubuntu:wght@400;500;700&display=swap');
 
     .container {        
         max-width: 1440px;           
-        background: linear-gradient(90.31deg, rgba(254, 134, 110, 0.77) 34.65%, #FF555E 99.72%);    
-        margin-left: auto;
-        margin-right: auto;
-        border-bottom-left-radius: 50px;
-    }    
+        background: linear-gradient(90.31deg, rgba(254, 134, 110, 0.77) 34.65%, #FF555E 99.72%);
+        margin: auto;
+        border-bottom-left-radius: 90px ;
+    } 
     nav{
-        display: flex;                       
-        color: white;        
+        display: grid;
+        grid-template-columns: .5fr 2fr .5fr;
+        /* display: flex;
+        justify-content: space-between; */
         align-items: center;
-        justify-content: flex-start;
+        padding:40px 60px;        
     }
-    .logo{                
-        font-size: 25px;
-        font-family: 'Overpass', sans-serif;        
-        order: 1; 
-        margin-left: 8vw;
+    .logo{
+        order: 1;
+        grid-column: 1/2;
+        justify-self: center;
+
     }
     ul{
-        justify-self: flex-start;
-        order: 2;
-        width: 40%;
+        display: flex;
+        order: 1;
+        justify-self: start;
+        grid-column: 2/3;
+        
         
     }
-    li{        
-        display: inline;
+    ul li{
+        list-style: none;
+        margin-left: 5px;
+    }
+    ul li a{
+        font-family: 'Overpass', sans-serif;
+        color: white;
+        margin-right: 5px;
         font-size: 12px;
-        float: left;           
+        padding-left: 20px;
+        
     }
     .btn-nav{
-        display: inline;
-        order: 3; 
-        margin-right: 1em;   
+        order: 2;
+        display: flex;
+        grid-column: 3/4;
+        justify-self: start;
     }
     .btn-nav button{
-        color: white;
         background: none;
-        border: 0;
-        width: 90px;
+        width: 100px;
+        height: 35px;
         border-radius: 50px;
+        border: none;
     }
     .btn-nav button:hover{
-        background: white;
-        color: #FF555E;
+        background: white;        
+    }
+    .btn-nav button a{
+        color: white;
+        font-family: 'Overpass', sans-serif;
+        font-size: 12px;
+        font-weight: 600;
+        text-decoration: none;
+    }
+    .btn-nav button a:hover{
+        font-size: 13px;
+        color: orange;
+        display:block
     }
     main{
-        display: flex;
-        justify-content: center;
-        flex-wrap: wrap;
-    }
-    h1{        
-        color: white;        
-    }
-    p{
+        padding: 1em;
         color: white;
-        text-align: center;
+    }
+    main h1{
+        display: flex;        
+        justify-content: center;
+        padding-bottom: 20px;
+        font-size: 50px;
+        font-weight: 500;
         
+    }
+    main p{
+        display: flex;        
+        justify-content: center;
+        font-size: 20px;
+        font-weight: 300;
+        padding-bottom: 20px;
     }
     .btn-botton{
-
-        
+        display: flex;        
+        justify-content: center;
+        padding-bottom: 80px;       
     }
+    .btn-botton button{
+        background: none;
+        width: 130px;
+        height: 40px;
+        border-radius: 50px;
+        border: 1px solid white;
+        margin: 10px;        
+    }
+    .btn-botton button a{
+        color: white;
+        font-family: 'Overpass', sans-serif;
+        font-size: 12px;
+        font-weight: 600;
+        text-decoration: none;
+    }
+
 
 
 
